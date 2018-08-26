f7f4fc1f060e35132f7b8d0acb07898bd0e3169c
Wellton S. Barros <welltonbarros@softbox.com.br>
2018-08-26 02:19:11 +0200

    Initial configuration for spring boot

---
+++ src/main/java/br/com/softplan/dudebase/DudeBaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.softplan.dudebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DudeBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DudeBaseApplication.class, args);
+    }
+
+}
